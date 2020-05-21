bbcf72b3126820fb5fc9d6128a601277a4103c87
eythanSaillet <eythan.saillet@gmail.com>
2020-05-21 02:26:49 +0200

    :sparkles: adding name input

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function NameInput() {
+	const [name, setName] = useState('')
+	const [fullBlockIsVisible, setFullBlockIsVisible] = useState(true)
+	let fullBlockFlash
+	const acceptedCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+	function onKeyPress(_event) {
+		if (_event.code === 'Backspace') {
+			setName((name) => name.substr(0, name.length - 1))
+		} else if (acceptedCharacters.includes(_event.key)) {
+			setName((name) => name + _event.key)
+		}
+	}
+
+	useEffect(() => {
+		if (window) {
+			window.addEventListener('keydown', onKeyPress)
+
+			fullBlockFlash = setInterval(() => {
+				setFullBlockIsVisible((fullBlockIsVisible) => !fullBlockIsVisible)
+			}, 750)
+		}
+		return () => {
+			window.removeEventListener('keydown', onKeyPress)
+			clearInterval(fullBlockFlash)
+		}
+	}, [])
+
+	return (
+		<div className="nameInputContainer">
+			<p className="nameInputLabel">Choose a name</p>
+			<p className="nameInput">{name}</p>
+			<div className={'fullBlockCharacter ' + (fullBlockIsVisible ? '' : 'invisible')}></div>
+		</div>
+	)
 }
 
-export default App;
+function App() {
+	return <NameInput />
+}
+
+export default App
