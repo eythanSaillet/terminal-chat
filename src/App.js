87b12a0e4ab29398891057e104f4f55332dac2fb
eythanSaillet <eythan.saillet@gmail.com>
2020-05-22 14:58:26 +0200

    :art: improving code and structure of NameInput component

--- src/App.js
+++ src/App.js
@@ -1,42 +1,7 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import './App.css'
 
-function NameInput() {
-	const [name, setName] = useState('')
-	const [fullBlockIsVisible, setFullBlockIsVisible] = useState(true)
-	let fullBlockFlash
-	const acceptedCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-	function onKeyPress(_event) {
-		if (_event.code === 'Backspace') {
-			setName((name) => name.substr(0, name.length - 1))
-		} else if (acceptedCharacters.includes(_event.key)) {
-			setName((name) => name + _event.key)
-		}
-	}
-
-	useEffect(() => {
-		if (window) {
-			window.addEventListener('keydown', onKeyPress)
-
-			fullBlockFlash = setInterval(() => {
-				setFullBlockIsVisible((fullBlockIsVisible) => !fullBlockIsVisible)
-			}, 750)
-		}
-		return () => {
-			window.removeEventListener('keydown', onKeyPress)
-			clearInterval(fullBlockFlash)
-		}
-	}, [])
-
-	return (
-		<div className="nameInputContainer">
-			<p className="nameInputLabel">Choose a name</p>
-			<p className="nameInput">{name}</p>
-			<div className={'fullBlockCharacter ' + (fullBlockIsVisible ? '' : 'invisible')}></div>
-		</div>
-	)
-}
+import NameInput from './components/NameInput'
 
 function App() {
 	return <NameInput />
---
+++ src/components/NameInput.js
@@ -0,0 +1,80 @@
+import React, { useState, useEffect } from 'react'
+import styled from 'styled-components'
+
+// Style
+
+const NameInputContainer = styled.div`
+	width: 300px;
+	height: 100px;
+	border: 2px solid var(--green);
+	position: relative;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	.nameInputLabel {
+		position: absolute;
+		top: -23px;
+		left: 20px;
+		background-color: var(--backGroundColor);
+		padding: 15px;
+		color: var(--green);
+	}
+
+	.nameInput {
+		font-size: 25px;
+	}
+
+	.fullBlockCharacter {
+		height: 23px;
+		width: 12px;
+		margin: 1px;
+		background-color: white;
+		transform: translateY(-3px);
+	}
+`
+
+const FullBlockCharacter = styled.div`
+	opacity: ${(props) => (props.invisibility ? 0 : 1)};
+`
+
+// Component
+
+function NameInput() {
+	const [name, setName] = useState('')
+	const [fullBlockIsVisible, setFullBlockIsVisible] = useState(true)
+	let fullBlockFlashID
+	const acceptedCharacters = 'abcdefghijklmnopqrstuvwxyz'
+
+	function onKeyPress(_event) {
+		if (_event.code === 'Backspace') {
+			setName((name) => name.substr(0, name.length - 1))
+		} else if (acceptedCharacters.indexOf(_event.key.toLowerCase()) !== -1) {
+			setName((name) => name + _event.key)
+			console.log(name)
+		}
+	}
+
+	useEffect(() => {
+		if (window) {
+			window.addEventListener('keydown', onKeyPress)
+
+			fullBlockFlashID = setInterval(() => {
+				setFullBlockIsVisible((fullBlockIsVisible) => !fullBlockIsVisible)
+			}, 750)
+		}
+		return () => {
+			window.removeEventListener('keydown', onKeyPress)
+			clearInterval(fullBlockFlashID)
+		}
+	}, [])
+
+	return (
+		<NameInputContainer>
+			<p className="nameInputLabel">Choose a name</p>
+			<p className="nameInput">{name}</p>
+			<FullBlockCharacter className="fullBlockCharacter" invisibility={fullBlockIsVisible}></FullBlockCharacter>
+		</NameInputContainer>
+	)
+}
+
+export default NameInput
